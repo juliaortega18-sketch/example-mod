901283ec9350c7e6ab9e59897ac58448363e7a00
juliaortega18-sketch <juliaortega18@gmail.com>
2026-06-25 15:04:41 -0400

    Implement game hacks and Mod Menu interface
    Added hooks for God Mode, No Clip, and Anti-Crash features in the game. Integrated a custom Mod Menu using ImGui for toggling these features.

--- src/main.cpp
+++ src/main.cpp
@@ -1,100 +1,111 @@
-/**
- * Include the Geode headers.
- */
 #include <Geode/Geode.hpp>
+#include <Geode/modify/PlayLayer.hpp>
+#include <Geode/modify/MenuLayer.hpp>
+#include <imgui.h>
 
-/**
- * Brings cocos2d and all Geode namespaces to the current scope.
- */
 using namespace geode::prelude;
 
-/**
- * `$modify` lets you extend and modify GD's classes.
- * To hook a function in Geode, simply $modify the class
- * and write a new function definition with the signature of
- * the function you want to hook.
- *
- * Here we use the overloaded `$modify` macro to set our own class name,
- * so that we can use it for button callbacks.
- *
- * Notice the header being included, you *must* include the header for
- * the class you are modifying, or you will get a compile error.
- *
- * Another way you could do this is like this:
- *
- * struct MyMenuLayer : Modify<MyMenuLayer, MenuLayer> {};
- */
-#include <Geode/modify/MenuLayer.hpp>
-class $modify(MyMenuLayer, MenuLayer) {
-	/**
-	 * Typically classes in GD are initialized using the `init` function, (though not always!),
-	 * so here we use it to add our own button to the bottom menu.
-	 *
-	 * Note that for all hooks, your signature has to *match exactly*,
-	 * `void init()` would not place a hook!
-	*/
-	bool init() {
-		/**
-		 * We call the original init function so that the
-		 * original class is properly initialized.
-		 */
-		if (!MenuLayer::init()) {
-			return false;
-		}
+// Variables globales para guardar el estado de los hacks
+bool g_godMode = false;
+bool g_antiCrash = false;
+bool g_noClip = false;
+bool g_showMenu = false;
 
-		/**
-		 * You can use methods from the `geode::log` namespace to log messages to the console,
-		 * being useful for debugging and such. See this page for more info about logging:
-		 * https://docs.geode-sdk.org/tutorials/logging
-		*/
-		log::debug("Hello from my MenuLayer::init hook! This layer has {} children.", this->getChildrenCount());
+// ==========================================
+// 1. LÓGICA DE LOS HACKS (Hooks de PlayLayer)
+// ==========================================
+class $modify(MyPlayLayer, PlayLayer) {
+    
+    // Hack: God Mode / No Clip
+    void destroyPlayer(PlayerObject* player, GameObject* object) {
+        // Si God Mode o No Clip están activados, ignoramos el daño
+        if (g_godMode || g_noClip) {
+            return;
+        }
+        // Si no, el juego sigue su curso normal y el jugador muere
+        PlayLayer::destroyPlayer(player, object);
+    }
 
-		/**
-		 * See this page for more info about buttons
-		 * https://docs.geode-sdk.org/tutorials/buttons
-		*/
-		auto myButton = CCMenuItemSpriteExtra::create(
-			CCSprite::createWithSpriteFrameName("GJ_likeBtn_001.png"),
-			this,
-			/**
-			 * Here we use the name we set earlier for our modify class.
-			*/
-			menu_selector(MyMenuLayer::onMyButton)
-		);
-
-		/**
-		 * Here we access the `bottom-menu` node by its ID, and add our button to it.
-		 * Node IDs are a Geode feature, see this page for more info about it:
-		 * https://docs.geode-sdk.org/tutorials/nodetree
-		*/
-		auto menu = this->getChildByID("bottom-menu");
-		menu->addChild(myButton);
-
-		/**
-		 * The `_spr` string literal operator just prefixes the string with
-		 * your mod id followed by a slash. This is good practice for setting your own node ids.
-		*/
-		myButton->setID("my-button"_spr);
-
-		/**
-		 * We update the layout of the menu to ensure that our button is properly placed.
-		 * This is yet another Geode feature, see this page for more info about it:
-		 * https://docs.geode-sdk.org/tutorials/layouts
-		*/
-		menu->updateLayout();
-
-		/**
-		 * We return `true` to indicate that the class was properly initialized.
-		 */
-		return true;
-	}
-
-	/**
-	 * This is the callback function for the button we created earlier.
-	 * The signature for button callbacks must always be the same,
-	 * return type `void` and taking a `CCObject*`.
-	*/
-	void onMyButton(CCObject*) {
-		FLAlertLayer::create("Geode", "Hello from my custom mod!", "OK")->show();
-	}
+    // Hack: Anti-Crash básico
+    void update(float dt) {
+        // Evitamos que un delta time corrupto o un lagazo extremo crasheen el juego
+        if (g_antiCrash && dt > 2.0f) {
+            dt = 1.0f / 60.0f; 
+        }
+        PlayLayer::update(dt);
+    }
 };
+
+// ==========================================
+// 2. INTERFAZ DEL MOD MENU (ImGui)
+// ==========================================
+// Esta función dibuja el menú flotante en la pantalla
+void drawModMenu() {
+    if (!g_showMenu) return;
+
+    // Creamos la ventana de nuestro Mod Menu
+    ImGui::Begin("Oc Blox Menu v1.0", &g_showMenu);
+
+    ImGui::Text("¡Bienvenido a tu Mod Menu!");
+    ImGui::Separator();
+
+    // Checkboxes para activar o desactivar cada hack
+    if (ImGui::Checkbox("God Mode", &g_godMode)) {
+        FLAlertLayer::create("Mod Menu", g_godMode ? "God Mode Activado" : "God Mode Desactivado", "OK")->show();
+    }
+
+    if (ImGui::Checkbox("No Clip", &g_noClip)) {
+        log::info("No Clip cambiado a: {}", g_noClip);
+    }
+
+    if (ImGui::Checkbox("Anti-Crash", &g_antiCrash)) {
+        log::info("Anti-Crash cambiado a: {}", g_antiCrash);
+    }
+
+    ImGui::Separator();
+    if (ImGui::Button("Cerrar Menú")) {
+        g_showMenu = false;
+    }
+
+    ImGui::End();
+}
+
+// ==========================================
+// 3. INYECCIÓN DEL BOTÓN EN EL MENÚ PRINCIPAL
+// ==========================================
+class $modify(MyMenuLayer, MenuLayer) {
+    bool init() {
+        if (!MenuLayer::init()) return false;
+
+        // Creamos un botón personalizado en la pantalla de inicio para abrir el menú
+        auto sprite = CCSprite::createWithSpriteFrameName("GJ_plusBtn_001.png"); // Botón con icono de '+'
+        auto btn = CCMenuItemSpriteExtra::create(
+            sprite,
+            this,
+            menu_selector(MyMenuLayer::onOpenModMenu)
+        );
+
+        // Buscamos el menú del juego para añadir nuestro botón
+        auto menu = this->getChildByID("bottom-menu");
+        if (menu) {
+            menu->addChild(btn);
+            menu->updateLayout();
+        }
+
+        return true;
+    }
+
+    // Función que se ejecuta al presionar el botón de '+'
+    void onOpenModMenu(CCObject* sender) {
+        // Cambiamos el estado (si estaba cerrado se abre, y viceversa)
+        g_showMenu = !g_showMenu;
+    }
+};
+
+// ==========================================
+// 4. REGISTRO DE LAPAFA DE IMGUI (Geode)
+// ==========================================
+// Le decimos a Geode que dibuje nuestra interfaz de ImGui en cada frame
+$execute {
+    ImGuiLayer::get()->addRenderCallback(&drawModMenu);
+}
